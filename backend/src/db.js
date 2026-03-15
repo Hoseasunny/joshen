13a7767e3c43bd15d2086a8bf34194dc223c7788
Hoseasunny <hoseasunny69@gmail.com>
2026-03-16 01:22:30 +0300

    removal (port 3000)

--- backend/src/db.js
+++ backend/src/db.js
@@ -3,10 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-if (process.env.DATABASE_URL) {
-  console.log("DATABASE_URL loaded");
-}
-
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set. Check backend/.env");
 }
